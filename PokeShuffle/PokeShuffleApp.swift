292e413d4dbcf0d32c2c4b8983e226d551126484
Alex Navarro <ajnavarro1424@gmail.com>
2023-01-05 09:20:16 -0800

    Initial Commit

---
+++ PokeShuffle/PokeShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeShuffleApp.swift
+//  PokeShuffle
+//
+//  Created by Alex Navarro on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
