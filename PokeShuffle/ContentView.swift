1c15634b580995727a073731743102938d6c5528
Alex Navarro <ajnavarro1424@gmail.com>
2023-01-05 10:03:59 -0800

    Initial commit
    - Built content view structure

--- PokeShuffle/ContentView.swift
+++ PokeShuffle/ContentView.swift
@@ -9,18 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView()
+            VStack() {
+                TitleTextView()
+                PokeImageView(pokemonImage: "questionmark")
+                Button(action: {
+
+                }, label: {
+                    ButtonView()
+                })
+                Spacer()
+
+            }
+            .padding(.top, 10)
         }
-        .padding()
-    }
+
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        LinearGradient(colors: [Color("pokeYellow"), Color("pokeBlue")],
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct TitleTextView: View {
+    var body: some View {
+        Text("PokeShuffle")
+            .foregroundColor(.white)
+            .font(.system(size: 42, weight: .medium, design: .default))
+    }
+}
+
+struct PokeImageView: View {
+    var pokemonImage: String
+
+    var body: some View {
+        Image(systemName: pokemonImage)
+            .renderingMode(.original)
+            .symbolRenderingMode(.monochrome)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 150, height: 150)
+            .padding(.top, 150)
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        Text("Shuffle")
+            .frame(width: 280, height: 50)
+            .foregroundColor(Color("pokeNavyBlue"))
+            .background(.white)
+            .font(.system(size: 25, weight: .medium, design: .default))
+            .cornerRadius(30)
+            .padding(.top, 150)
+
     }
 }
